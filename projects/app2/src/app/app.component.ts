6fd23227a9100a0a51fec4b17e84cefe677d32c4
unknown <vikasa.agarwal@gmail.com>
2022-01-06 19:16:54 +0530

    added iframe parent child communication

--- projects/app2/src/app/app.component.ts
+++ projects/app2/src/app/app.component.ts
@@ -1,10 +1,26 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app2';
+
+  ngOnInit() {
+    window.addEventListener('message', (event) => {
+      // IMPORTANT: check the origin of the data!
+      if (event.origin.startsWith('http://localhost:63247')) {
+        // The data was sent from your site.
+        // Data sent with postMessage is stored in event.data:
+        console.log('Received data:', event.data);
+      } else {
+        // The data was NOT sent from your site!
+        // Be careful! Do not use it. This else branch is
+        // here just for clarity, you usually shouldn't need it.
+        return;
+      }
+    });
+  }
 }
--- projects/parentapp/src/app/app.component.ts
+++ projects/parentapp/src/app/app.component.ts
@@ -11,9 +11,19 @@ export class AppComponent {
   @ViewChild('frm1')
   frame1!: any;
 
+  @ViewChild('frm2')
+  frame2!: any;
+
   play() {
     console.log(this.frame1);
-    (window as any).frame1 = this.frame1;
+    (window as any).frame1 = this.frame1.nativeElement;
+    (window as any).frame2 = this.frame2.nativeElement;
+  }
+
+  sendMessageToApp2() {
+    if((window as any).frame2) {
+      (window as any).frame2.contentWindow.postMessage('sendapp2', 'http://localhost:63091'); 
+    }
   }
 
 }
